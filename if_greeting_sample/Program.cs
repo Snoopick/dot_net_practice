ec19b550766fcce86837647cbf3d153c6e687552
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-09-26 20:31:41 +0300

    lesson work 1

---
+++ if_greeting_sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace if_greeting_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan nowTime = DateTime.Now.TimeOfDay;
+
+            int nowHour = nowTime.Hours;
+
+
+            if (nowHour > 9 && nowHour < 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (nowHour > 12 && nowHour < 15)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else if (nowHour > 15 && nowHour < 22)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+            else if (nowHour > 22 && nowHour < 9) {
+                Console.WriteLine("Good morning, guys");
+            }
+        }
+    }
+}
