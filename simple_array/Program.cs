e584741efdecc439d4dbff6a60e61d7874e9c342
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-10-01 21:27:45 +0300

    simple array

---
+++ simple_array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace simple_array {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = new int[6];
+
+            for (int i = 0; i < array.Length; i++) {
+                Console.WriteLine("Enter num");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++) {
+                Console.WriteLine(array[i]);
+            }
+
+
+            Console.WriteLine("Changed array");
+            for (int i = 0; i < array.Length; i++) {
+                if (i == 2) {
+                    array[i] *= 10;
+                }
+
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
