b1a45e86b2fadb042468db14401377ec253d2439
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-10-17 21:51:06 +0300

    class work

---
+++ lesson_10/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task2 {
+    class Program {
+        static void Main(string[] args) {
+            UniqueItem test1 = new UniqueItem();
+            UniqueItem test2 = new UniqueItem();
+            UniqueItem test3 = new UniqueItem(5);
+            UniqueItem test4 = new UniqueItem();
+            UniqueItem test5 = new UniqueItem();
+
+            Console.WriteLine(test5.getNum());
+        }
+    }
+
+    class UniqueItem {
+        public int _counter;
+
+        public UniqueItem(int counter = 0) {
+            if (counter != 0) {
+                this._counter = counter;
+            }
+
+            this._counter++;
+        }
+
+        public int getNum() {
+            return this._counter;
+        }
+    }
+}
