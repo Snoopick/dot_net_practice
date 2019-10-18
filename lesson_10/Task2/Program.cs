5d341a7ff1e4e49839f3fa19c9813776061fb77c
Alexey <ap@imarket.by>
2019-10-18 09:20:22 +0300

    task 2

--- lesson_10/Task2/Program.cs
+++ lesson_10/Task2/Program.cs
@@ -1,31 +1,37 @@
 ﻿using System;
 
 namespace Task2 {
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        public static int iterator;
+
+        static void Main(string[] args)
+        {
             UniqueItem test1 = new UniqueItem();
             UniqueItem test2 = new UniqueItem();
             UniqueItem test3 = new UniqueItem(5);
             UniqueItem test4 = new UniqueItem();
             UniqueItem test5 = new UniqueItem();
-
-            Console.WriteLine(test5.getNum());
         }
     }
 
-    class UniqueItem {
-        public int _counter;
+    class UniqueItem
+    {
+        private int _counter;
 
-        public UniqueItem(int counter = 0) {
-            if (counter != 0) {
-                this._counter = counter;
+        public UniqueItem(int counter = 0)
+        {
+            this._counter = Program.iterator;
+
+            if (counter != 0)
+            {
+                this._counter += counter;
             }
 
             this._counter++;
-        }
+            Program.iterator = this._counter;
 
-        public int getNum() {
-            return this._counter;
+            Console.WriteLine("Num is " + this._counter);
         }
     }
 }
