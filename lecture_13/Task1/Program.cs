8fca9ffcd63a41d82903ebb31c25b57234a0e701
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-10-24 20:10:59 +0300

    task 1

---
+++ lecture_13/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1 {
+    class Program {
+        static void Main(string[] args) {
+            Song song = new Song();
+            song.songAuthor = "Song author";
+            song.songLen = 231;
+            song.songName = "Song 1";
+            song.songDate = DateTime.Now;
+
+            Console.WriteLine(song.GetSongData(song));
+        }
+    }
+}
---
+++ lecture_13/Task1/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1 {
+    class Song {
+        public string songName = string.Empty;
+        public double songLen = 0.0;
+        public string songAuthor = string.Empty;
+        public DateTime songDate = new DateTime();
+
+        public object GetSongData(Song song) {
+            var songData = new {
+                Title = song.songName,
+                Minutes = (song.songLen / 60),
+                AlbumYear = song.songDate
+            };
+
+            return songData;
+        }
+    }
+}
